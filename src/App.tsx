c62301ad8d34069fbfb5c7053b4a3bbd5536da17
Godly-MysteriOus <singhrajputjayant8@gmail.com>
2026-05-23 00:15:37 +0530

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { createTheme } from '@mui/material/styles';
+
+function App() {
+  
+  const defaultTheme = createTheme();
+  console.log('Full MUI Theme Object:', defaultTheme);
+  return (<></>)
+}
+
+export default App
