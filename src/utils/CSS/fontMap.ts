47224175742a1bc68b7c9eb71e0b4f3a04acd3e0
Godly-MysteriOus <singhrajputjayant8@gmail.com>
2026-05-26 01:58:32 +0530

    Adding AG Grid component

---
+++ src/utils/CSS/fontMap.ts
@@ -0,0 +1,8 @@
+const fontFamilyList = {
+  poppins:'"Poppins", sans-serif',
+  montserrat : '"Montserrat", sans-serif',
+  noto : '"Noto Sans TC", sans-serif',
+  lato: '"Lato", "sans-serif"'
+}
+
+export default fontFamilyList;
